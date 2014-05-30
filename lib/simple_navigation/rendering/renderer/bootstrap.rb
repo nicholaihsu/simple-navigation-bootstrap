dcc3c1daff54389af648e9e1a0510c7d6691859c
Nicholai Hsu <nicholai@agilecredit.com>
2014-05-31 02:53:40 +0800

    Disable bootstrap caret

--- lib/simple_navigation/rendering/renderer/bootstrap.rb
+++ lib/simple_navigation/rendering/renderer/bootstrap.rb
@@ -56,7 +56,7 @@ module SimpleNavigation
             item_options[:link][:class] << 'dropdown-toggle'
             item_options[:link][:'data-toggle'] = 'dropdown'
             item_options[:link][:'data-target'] = '#'
-            link << content_tag(:b, '', :class => 'caret')
+            #link << content_tag(:b, '', :class => 'caret')
           end
           item.html_options = item_options
         end
